6b6d6245a71e446d93417b94aa211e6170082281
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2018-03-22 10:34:15 +0100

    beschrijving

--- network.go
+++ network.go
@@ -12,11 +12,31 @@ import (
 	"strings"
 	"github.com/trivento/network/iptools"
 )
+
+/*
+Opdracht:
+
+Implementeer een netwerk cluster member die zich kan aanmelden bij een cluster van Nodes en
+actief (gossippen van kennis over het cluster) participeert.
+
+- Gossip protocol:
+	1. Meld je aan bij de Seed node door een lijst van known nodes met enkel jezelf te sturen
+	2. Periodiek naar een aantal nodes in de lijst van members jouw kennis van het netwerk sturen
+	3. Als je een gossip/lijst van nodes ontvangt, dan voeg je alle nieuwe nodes toe aan je eigen cluster kennis
+- Data structuur van de informatie die ge-gossiped wordt:
+	vb: {"nodes":["http://10.248.30.150:8082","http://10.248.30.143:8081"]}
+
+
+ */
+
+
+// Data structuur
 //{"nodes":["http://10.248.30.150:8082","http://10.248.30.150:8081"]}
 type members struct {
 	Nodes []string `json:"nodes"`
 }
 
+// Maak een store waarin alle members gezet worden. Tip: Go kent geen Set
 var store = make(map[string]bool)
 
 func addMember(member string) {
